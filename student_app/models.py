5e89c3453b35775d883b1edc06e9531c3622580f
dlibberton <drew.libberton@gmail.com>
2024-03-19 14:58:46 -0400

    updated models and added methods to update fields passing tests

--- student_app/models.py
+++ student_app/models.py
@@ -2,9 +2,21 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    name = models.CharField(max_length=100)
-    student_email = models.EmailField()
-    personal_email = models.EmailField(blank=True, null=True)
-    locker_number = models.IntegerField()
-    locker_combination = models.CharField(max_length=20)
-    good_student = models.BooleanField(default=False)
+    name = models.CharField(max_length=100, unique=False)
+    student_email = models.EmailField(unique=True)
+    personal_email = models.EmailField(unique=True, blank=True, null=True)
+    locker_number = models.IntegerField(unique=True, default=110)
+    locker_combination = models.CharField(max_length=20, unique=False, default="12-12-12")
+    good_student = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return f"{self.name} - {self.student_email} - {self.locker_number}"
+    
+    def locker_reassignment(self, new_locker_number):
+        self.locker_number = new_locker_number
+        self.save()
+        
+    def student_status(self, is_good_student):
+        self.good_student = is_good_student
+        self.save()
+        
